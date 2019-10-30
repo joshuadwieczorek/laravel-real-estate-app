c8e3697e41342388b2e46f3ce39f720ea888dd62
Joshua Wieczorek <joshuawieczorek@outlook.com>
2019-10-29 22:09:05 -0400

    more updates to the listings service

---
+++ app/Http/Responses/ListingsResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Responses;
+use App\Core\ResponseBase;
+use App\Eloquent\Listing;
+use App\Models\ListingModel;
+use App\Models\ListingImageModel;
+
+class ListingsAllResponse extends ResponseBase
+{
+
+	
+
+
+	/**
+	 * Prepare data.
+	 *
+	 * @return mixed
+	 */
+	public function Prepare()
+	{
+		// TODO: Implement Prepare() method.
+	}
+}
